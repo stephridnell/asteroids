46524d99cc85aae30214bb9df472e43678ff7ce2
steph ridnell <steph.ridnell@gmail.com>
2025-03-05 19:13:25 +1100

    Added clock

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ def main():
   screen.fill((0, 0, 0))
   pygame.display.flip()
 
+  clock = pygame.time.Clock()
+  dt = 0
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    
+    time_passed = clock.tick(FRAMES_PER_SECOND)
+    dt += time_passed / 1000
   
 
 if __name__ == "__main__":
